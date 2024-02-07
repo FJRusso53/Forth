53c30714630efb2361693a509733a9f4e306659f
Frank Russo <91231875+FJRusso53@users.noreply.github.com>
2024-02-07 12:02:15 -0500

    Add files via upload

---
+++ autoexec.fs
@@ -0,0 +1,16 @@
+( Block 0 )                                                    
+cr .vs .id cr                                                
+also internals also esp                                        
+150000 getfreeheap - relinquish                                
+getfreeheap ." Free Heap = " . cr                              
+spiffs.ldir                                                    
+                                                               
+                                                               
+                                                               
+                                                               
+                                                               
+                                                               
+                                                               
+                                                               
+                                                               
+                                                               
